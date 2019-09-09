282938523e8890a3c8183e7aeacb6d49427a6a27
Cristian <fulanito@gmail.com>
2019-09-09 01:03:43 -0300

    arreglar movimento lateral, resize y acciones. Ademas de Sprites

--- src/game.h
+++ src/game.h
@@ -6,11 +6,7 @@ class Game
 public:
 /*Constructor*/
 	Game();
-    //Walking animation
-const int WALKING_ANIMATION_FRAMES = 4;
-SDL_Rect gSpriteClips[ 4 ];
-SDL_Texture* gSpriteSheetTexture;
-
+    
 private:
     class Window* _gwindow;
     void initialize();
