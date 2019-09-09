514785a4e95a56220a9802382541ab1958e47d68
Axel <axelmpm@gmail.com>
2019-09-09 00:52:02 -0300

    1

--- src/testing_sdl.cpp
+++ src/testing_sdl.cpp
@@ -64,10 +64,10 @@ bool loadMedia()
 	bool success = true;
 
 	//Load splash image
-	gHelloWorld = SDL_LoadBMP( "bin/hello_world.bmp" );
+	gHelloWorld = SDL_LoadBMP( "02_getting_an_image_on_the_screen/hello_world.bmp" );
 	if( gHelloWorld == NULL )
 	{
-		printf( "Unable to load image %s! SDL Error: %s\n", "bin/hello_world.bmp", SDL_GetError() );
+		printf( "Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError() );
 		success = false;
 	}
 
