49f0403b32314734091a49060e4d63345e5a0a81
Tomás Macía <tomas.macia@mercadolibre.com>
2019-09-08 23:14:09 -0300

    refactor main

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,20 @@
 #include "parser/xmlparser.h"
 using namespace std; 
 
-int main(int argc, char** argv) { 
-    cout << "Hello World 1.0" << "\n";
+int main(int argc, char** argv) {
+    string pathToFile;
 
     if (argc >= 3 && (string(argv[1]) == "-c" || string(argv[1]) == "--config")){
-        string pathToFile = argv[2];
-        XMLParser parser;
-        Config config = parser.parse(pathToFile);
-        cout << "There are " << config.gameplay.levels.size() << " levels in the game" << "\n";
-        cout << "Logger level set in " << config.loggerLevel << "\n";
+        pathToFile = argv[2];
     } else {
-        cerr << "USAGE: " << argv[0] << " --config path/to/config/file.xml" << "\n";
+        cerr << "No config file specified." << endl <<
+        "USAGE: " << argv[0] << " --config path/to/config/file.xml" << endl;
     }
+
+    XMLParser parser;
+    Config config = parser.parse(pathToFile);
+    cout << "There are " << config.gameplay.levels.size() << " levels in the game" << endl;
+    cout << "Logger level set in " << config.loggerLevel << endl;
   
     return 0; 
 } 
