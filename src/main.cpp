b4ed154cce69c1a1ab0b88c65dfb61e3372c68c1
Cristian <fulanito@gmail.com>
2019-09-17 09:07:22 -0300

    quitando un poco de codigo repetido

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@ using namespace std;
 
 #include <iostream> 
 #include "game.h"
-#include <vector>
+
   
 int main(int argc, char** argv) { 
 
@@ -10,18 +10,3 @@ int main(int argc, char** argv) {
     return 0;
 
 } 
-
-void level1(){
-    std::vector< string> g1;
-    g1.push_back("Sprites/FF_Stage4_floor6.png");
-    g1.push_back("Sprites/FF_Stage4_floor5.png");
-    g1.push_back("Sprites/FF_Stage4_floor4.png");
-    g1.push_back("Sprites/FF_Stage4_floor3.png");
-    g1.push_back("Sprites/FF_Stage4_floor2.png");
-    g1.push_back("Sprites/FF_Stage4_floor1.png");
-
-    for(int i=0;i<6;i++){
-       std::cerr<<g1[i]<<std::endl;
-    }
-    
-}
