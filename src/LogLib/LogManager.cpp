f63fdeafc67b8874fc04ca653b47df0c5ab71e91
Tomás Macía <tomas.macia@mercadolibre.com>
2019-09-22 18:34:07 -0300

    Prettier format Log output

--- src/LogLib/LogManager.cpp
+++ src/LogLib/LogManager.cpp
@@ -1,7 +1,6 @@
 #include "LogManager.h"
 #include "Logger.h"
 #include <fstream>
-#include <ctime>
 
 Logger* LogManager::loggerToBeUsed;
 bool LogManager::pathHasBeenSet;
@@ -14,9 +13,10 @@ void LogManager::setStaticLogger(Logger* logger){
 void LogManager::setStaticLogPath(std::string path){
     if(LogManager::pathHasBeenSet){
         LogManager::file.close();
-    };
-        file.open(path);
-        LogManager::pathHasBeenSet = true;
+    }
+
+    file.open(path);
+    LogManager::pathHasBeenSet = true;
 };
 
 void LogManager::logError(std::string message){
@@ -31,8 +31,21 @@ void LogManager::logDebug(std::string message){
     loggerToBeUsed->logDebug(message);
 };
 
-void LogManager::writeLogFile(std::string message){
-    time_t now = std::time(0);
-    file << std::ctime(&now) + message + '\n';
+void LogManager::writeLogFile(std::string message, std::string level){
+    file << getCurrentTime() + " [" + level + "] : " + message << std::endl;
 };
+
+std::string LogManager::getCurrentTime() {
+    time_t rawtime;
+    struct tm* timeinfo;
+    char buffer[80];
+
+    time (&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    strftime(buffer,sizeof(buffer),"%d-%m-%Y %H:%M:%S",timeinfo);
+    std::string str(buffer);
+
+    return str;
+}
 
