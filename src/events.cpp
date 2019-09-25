f427171416912fbff4c68823dbb82a90956f6902
FrancoR <33163113+francoriboratig@users.noreply.github.com>
2019-09-25 15:05:08 -0300

    Configurable bindings (#19)
    * teclas modificables pero no andan las flechitas
    
    * Arreglado teclas de movimiento

--- src/events.cpp
+++ src/events.cpp
@@ -4,76 +4,80 @@
 #include "character.h"
 /* Funcionales son los movimientos (no laterales)
 con las flechas, saltar con LCTRL, golpear con X y agacharse con Z*/
+
+map<SDL_Scancode,binding>* Events::hotkeys;
+
 bool Events::keyboard_event() {
     execute_event();
     if (SDL_PollEvent(&_event)) {
+        binding b = getBindingForKeyPress(_event.key.keysym.scancode);
         switch (_event.type) {
             /*Click en la X*/
             case SDL_QUIT:
                 LogManager::logDebug("Click en CERRAR (X)");
                 return true;
             case SDL_KEYDOWN:
-                switch (_event.key.keysym.scancode) {
-                    case(SDL_SCANCODE_ESCAPE):
+                switch (b) {
+                    case(QUIT):
                         /*Aprete escape*/
                         LogManager::logDebug("ESCAPE presionado");
                         return  true;
-                    case SDL_SCANCODE_LEFT:
+                    case LEFT:
                         left =true;
                         _cody->setFlip(SDL_FLIP_HORIZONTAL);
                         return false;
-                    case SDL_SCANCODE_RIGHT:
+                    case RIGHT:
                         rigth = true;
                         _cody->setFlip(SDL_FLIP_NONE);
                         // solo me interesa la derecha
                         return false;
-                    case SDL_SCANCODE_UP:
+                    case UP:
                         up = true;
                         return false;
-                    case SDL_SCANCODE_DOWN:
+                    case DOWN:
                         down = true;
                         return false;
-                    case SDL_SCANCODE_LCTRL:
+                    case JUMP:
                         //Aviso si se presiona rigth o left antes de saltar
                         if (rigth){_cody->mov_jump(0);}
                         if (left) {_cody->mov_jump(1);}
                         _cody->move(1,-1);
                         return false;
-                    case SDL_SCANCODE_X:
+                    case PUNCH:
                         _cody->move(2,-1);
                         return false;
-                    case SDL_SCANCODE_Z:
+                    case CROUCH:
                         _cody->move(3,-1);
                         return false;
-                    case SDL_SCANCODE_C:
+                    case JUMP_KICK:
                         //Aviso si se presiona rigth o left antes de salta
                         if (rigth){ _cody->mov_jump(0);}
                         if (left) {_cody->mov_jump(1);}
                         _cody->move(5,-1);
                         return false;
-                    case SDL_SCANCODE_A:
+                    case KICK:
                         _cody->move(6,-1);
                         return false;
                     default:
                         return false;
                 }
             case SDL_KEYUP:
-                switch (_event.key.keysym.scancode) {
-                    case SDL_SCANCODE_LEFT:
+                switch (b) {
+                    case LEFT:
                         left = false;
                         return false;
-                    case SDL_SCANCODE_RIGHT:
+                    case RIGHT:
                         rigth = false;
                         return false;
-                    case SDL_SCANCODE_UP:
+                    case UP:
                         up = false;
                         return false;
-                    case SDL_SCANCODE_DOWN:
+                    case DOWN:
                         down = false;
                         return false;
-                    case SDL_SCANCODE_X:
+                    case PUNCH:
                         return false;
-                        case SDL_SCANCODE_LCTRL:
+                    case JUMP:
                         return false;
                     default:
                         return false;
@@ -82,7 +86,7 @@ bool Events::keyboard_event() {
         return false;
     }
     return false;
-}
+};
 
 
 void Events::execute_event(){
@@ -92,4 +96,20 @@ void Events::execute_event(){
     if (rigth) {_cody->move(0,6);}
     if (left){_cody->move(0,4);}
     if(down){_cody->move(0,2);}
+}
+
+binding Events::getBindingForKeyPress(SDL_Scancode scancode){
+    try{
+        return Events::hotkeys->at(scancode);
+    } catch (const std::out_of_range){
+        return NONE;
+    }
+}
+
+void Events::addHotkey(SDL_Scancode scancode, binding b){
+    Events::hotkeys->insert(std::make_pair(scancode,b));
+}
+
+void Events::initHotkeys(){
+    Events::hotkeys = new map<SDL_Scancode,binding>;
 }
--- src/game.cpp
+++ src/game.cpp
@@ -22,6 +22,7 @@ void Game::init() {
     this->initConfig();
     this->initLogManager(this->config->loggerLevel);
     this->initSDL();
+    this->loadHotkeys(this->config);
 }
 
 void Game::initConfig() {
@@ -208,3 +209,168 @@ void Game::destroy() {
     SDL_DestroyRenderer(this->renderer);
     SDL_Quit();
 }
+
+void Game::loadHotkeys(Config* config){
+    Events::initHotkeys();
+    Events::addHotkey(stringToScancode(config->bindings.ATTACK),PUNCH);
+    Events::addHotkey(stringToScancode(config->bindings.CROUCH),CROUCH);
+    Events::addHotkey(stringToScancode(config->bindings.DOWN),DOWN);
+    Events::addHotkey(stringToScancode(config->bindings.JUMP),JUMP);
+    Events::addHotkey(stringToScancode(config->bindings.LEFT),LEFT);
+    Events::addHotkey(stringToScancode(config->bindings.RIGHT),RIGHT);
+    Events::addHotkey(stringToScancode(config->bindings.UP),UP);
+    Events::addHotkey(SDL_SCANCODE_ESCAPE,QUIT);
+    //Events::addHotkey(stringToScancode(config->bindings.JUMP_KICK),JUMP_KICK);
+    //Events::addHotkey(stringToScancode(config->bindings.KICK),KICK);
+};
+
+SDL_Scancode Game::stringToScancode(std::string s){
+
+    //Wellcome to hell
+
+    if ((s == "A") or (s == "a")){
+        return SDL_SCANCODE_A;
+    };
+    if ((s == "B") or (s == "b")){
+        return SDL_SCANCODE_B;
+    };
+    if ((s == "C") or (s == "c")){
+        return SDL_SCANCODE_C;
+    };
+    if ((s == "D") or (s == "d")){
+        return SDL_SCANCODE_D;
+    };
+    if ((s == "E") or (s == "e")){
+        return SDL_SCANCODE_E;
+    };
+    if ((s == "F") or (s == "f")){
+        return SDL_SCANCODE_F;
+    };
+    if ((s == "G") or (s == "g")){
+        return SDL_SCANCODE_G;
+    };
+    if ((s == "H") or (s == "h")){
+        return SDL_SCANCODE_H;
+    };
+    if ((s == "I") or (s == "i")){
+        return SDL_SCANCODE_I;
+    };
+    if ((s == "J") or (s == "j")){
+        return SDL_SCANCODE_J;
+    };
+    if ((s == "K") or (s == "k")){
+        return SDL_SCANCODE_K;
+    };
+    if ((s == "L") or (s == "l")){
+        return SDL_SCANCODE_L;
+    };
+    if ((s == "M") or (s == "m")){
+        return SDL_SCANCODE_M;
+    };
+    if ((s == "N") or (s == "n")){
+        return SDL_SCANCODE_N;
+    };
+    if ((s == "O") or (s == "o")){
+        return SDL_SCANCODE_O;
+    };
+    if ((s == "P") or (s == "p")){
+        return SDL_SCANCODE_P;
+    };
+    if ((s == "Q") or (s == "q")){
+        return SDL_SCANCODE_Q;
+    };
+    if ((s == "R") or (s == "r")){
+        return SDL_SCANCODE_R;
+    };
+    if ((s == "S") or (s == "s")){
+        return SDL_SCANCODE_S;
+    };
+    if ((s == "T") or (s == "t")){
+        return SDL_SCANCODE_T;
+    };
+    if ((s == "U") or (s == "u")){
+        return SDL_SCANCODE_U;
+    };
+    if ((s == "V") or (s == "v")){
+        return SDL_SCANCODE_V;
+    };
+    if ((s == "W") or (s == "w")){
+        return SDL_SCANCODE_W;
+    };
+    if ((s == "X") or (s == "x")){
+        return SDL_SCANCODE_X;
+    };
+    if ((s == "Y") or (s == "y")){
+        return SDL_SCANCODE_Y;
+    };
+    if ((s == "Z") or (s == "z")){
+        return SDL_SCANCODE_Z;
+    };
+    if ((s == "CONTROL") or (s == "LCTRL")){
+        return SDL_SCANCODE_LCTRL;
+    };
+    if (s == "RCTRL"){
+        return SDL_SCANCODE_RCTRL;
+    };
+    if ((s == "SHIFT") or (s == "LSHIFT")){
+        return SDL_SCANCODE_LSHIFT;
+    };
+    if (s == "RSHIFT"){
+        return SDL_SCANCODE_RSHIFT;
+    };
+    if ((s == "ALT") or (s == "LALT")){
+        return SDL_SCANCODE_LALT;
+    };
+    if (s == "SPACE"){
+        return SDL_SCANCODE_SPACE;
+    };
+    if (s == "UP"){
+        return SDL_SCANCODE_UP;
+    };
+    if (s == "LEFT"){
+        return SDL_SCANCODE_LEFT;
+    };
+    if (s == "RIGHT"){
+        return SDL_SCANCODE_RIGHT;
+    };
+    if (s == "DOWN"){
+        return SDL_SCANCODE_DOWN;
+    };
+    if (s == "1"){
+        return SDL_SCANCODE_KP_1;
+    };
+    if (s == "2"){
+        return SDL_SCANCODE_KP_2;
+    };
+    if (s == "3"){
+        return SDL_SCANCODE_KP_3;
+    };
+    if (s == "4"){
+        return SDL_SCANCODE_KP_4;
+    };
+    if (s == "5"){
+        return SDL_SCANCODE_KP_5;
+    };
+    if (s == "6"){
+        return SDL_SCANCODE_KP_6;
+    };
+    if (s == "7"){
+        return SDL_SCANCODE_KP_7;
+    };
+    if (s == "8"){
+        return SDL_SCANCODE_KP_8;
+    };
+    if (s == "9"){
+        return SDL_SCANCODE_KP_9;
+    };
+    if (s == "0"){
+        return SDL_SCANCODE_KP_0;
+    };
+    if (s == "ENTER"){
+        return SDL_SCANCODE_RETURN;
+    };
+    if ((s == "RALT") or ("ALTGR")){
+        return SDL_SCANCODE_RALT;
+    };
+    return SDL_SCANCODE_WWW;
+};
