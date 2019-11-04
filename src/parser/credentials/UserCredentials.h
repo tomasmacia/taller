723f82c558e53b8d397172a0c8370032d3133b7f
Tomás Macía <tomas.macia@mercadolibre.com>
2019-11-04 15:43:09 -0300

    credenciales

---
+++ src/parser/credentials/UserCredentials.h
@@ -0,0 +1,16 @@
+//
+// Created by Tomás Macía on 04/11/2019.
+//
+
+#ifndef GAME_USERCREDENTIALS_H
+#define GAME_USERCREDENTIALS_H
+
+#include <string>
+
+class UserCredentials {
+public:
+    std::string username;
+    std::string password;
+};
+
+#endif //GAME_USERCREDENTIALS_H
