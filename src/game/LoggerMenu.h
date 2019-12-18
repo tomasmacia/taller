c00885b7253b18ef7f270fe460f24de10a73b0c3
Tomás Macía <tomas.macia@mercadolibre.com>
2019-12-18 14:41:46 -0300

    possibililty to change punch delay for boss

--- src/game/LoggerMenu.h
+++ src/game/LoggerMenu.h
@@ -7,8 +7,8 @@
 #include <map>
 #include <iomanip>
 #include <SDL2/SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 
 #include "../CLIAparser/CLIArgumentParser.h"
 #include "config/config.h"
