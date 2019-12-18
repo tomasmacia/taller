967aa3d1d9ec5118c556a82282ffa2bf67c9c4ba
Tomás Macía <tomas.macia@mercadolibre.com>
2019-12-18 00:10:56 -0300

    do not serialize nulls

--- src/game/Game.h
+++ src/game/Game.h
@@ -2,8 +2,8 @@
 #define GAME_H_
 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_mixer.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
 
 #include "SceneDirector.h"
 #include "../logger/LogManager.h"
--- src/game/GameClient.cpp
+++ src/game/GameClient.cpp
@@ -50,13 +50,13 @@ void GameClient::gameLoop() {
         pollAndSendInput(); //aca se podria cortar el game loop si se lee un ESC o QUIT
 
         if ((disconnect || playerDied) && !endOfGame){
-            if (!youDiedMusicPlaying){
-
-                initYouDiedOrDisconnectedMusic();
-                gameMusic->play();
-                youDiedMusicPlaying = true;
-                normalGameMusicPlaying = false;
-            }
+//            if (!youDiedMusicPlaying){
+//
+//                initYouDiedOrDisconnectedMusic();
+//                gameMusic->play();
+//                youDiedMusicPlaying = true;
+//                normalGameMusicPlaying = false;
+//            }
         }
 
         else if (gameStarted){
@@ -72,12 +72,13 @@ void GameClient::gameLoop() {
             pauseMusic();
         }
 
-        if (disconnect && !endOfGame){
-            sceneDirector->renderDisconectionScreen(renderer, &loadedTexturesMap);
-        }
-        else {
-            render();
-        }
+//        if (disconnect && !endOfGame){
+//            sceneDirector->renderDisconectionScreen(renderer, &loadedTexturesMap);
+//        }
+//        else {
+//            render();
+//        }
+        render();
     }
 }
 
