159c24a102ec4e931674749d1358c891da2fbfe6
axel <axelmpm@gmail.com>
2019-12-21 14:52:21 -0300

    retoca un logeo de Error en sending y reading en Server y Client. Corrige un logeo de paths en el XMLParser y homegeiniza los paths en la config default para evitar problemas de mapeo en el caso default que deberia ser el mas estable

--- src/net/Server.cpp
+++ src/net/Server.cpp
@@ -60,7 +60,7 @@ int Server::send(string msg, int someSocketFD) {
 
             int n = ::send(someSocketFD, buff, MAX_BYTES_BUFFER - 1, MSG_NOSIGNAL);
             if (n < 0 && errno != EAGAIN) {
-                error("[SERVER] error sending | errno: " + to_string(errno));
+                error("error sending | errno: " + to_string(errno));
                 beginDisconectionWith(socketIDMap.at(someSocketFD));
                 return n;
             }
@@ -91,7 +91,7 @@ string Server::receive(int someSocketFD) {
                 return objectSerializer->getFailure();
             }
             else{
-                error("[SERVER] error reading | errno: " + to_string(errno));
+                error("error reading | errno: " + to_string(errno));
                 beginDisconectionWith(socketIDMap.at(someSocketFD));
             }
         }
@@ -109,8 +109,8 @@ string Server::receive(int someSocketFD) {
     string failureMessage = objectSerializer->getFailure();
     std::string parsed = messageParser.extractMeaningfulMessageFromStream(buff,MAX_BYTES_BUFFER, failureMessage, start, end,padding);
     if (parsed == failureMessage){
-        cout << "SERVER-READ: " << parsed << endl;
     }
+    cout << "SERVER-READ: " << parsed << endl;
     return parsed;
 }
 
